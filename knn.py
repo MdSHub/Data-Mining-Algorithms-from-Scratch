df614eee1d76635c3dcea9a49b4e41e475fa35d5
Mohd Saqib <saqibali9718620@gmail.com>
2020-02-06 11:15:49 +0530

    Update knn.py

--- knn.py
+++ knn.py
@@ -1,16 +1,3 @@
-n = {0:[(8,5),(7,3)], 
-              1:[(3,7)],
-              2:[(3,6)]
-             } 
-  
-    # testing point p(x,y)
-p = (3,4) 
-  
-    # Number of neighbours  
-k = 3
-  
-
-
 def KNNClassifier(n,p,k=3): 
     
     ApartDistance=[] 
@@ -18,7 +5,7 @@ def KNNClassifier(n,p,k=3):
         for clas in n[label]: 
   
              
-            Neighbour_distance = ((clas[0]-p[0])**2 +(clas[1]-p[1]))**0.5       #sqrtApply
+            Neighbour_distance = ((clas[0]-p[0])**2 +(clas[1]-p[1])**2+(clas[2]-p[2])**2)**0.5       #sqrtApply
   
              
             ApartDistance.append((Neighbour_distance,label)) 
@@ -39,7 +26,7 @@ def KNNClassifier(n,p,k=3):
         elif d[1] == 2:
             freq3 += 1
   
-    
+    #return 0 if freq1>freq2 else 1
     if(freq1>freq2 and freq1>freq3):
       return 0
     elif(freq2>freq1 and freq2>freq3):
@@ -47,8 +34,17 @@ def KNNClassifier(n,p,k=3):
     elif(freq3>freq1 and freq3>freq2):
       return 2
 
-
-
-
-a=KNNClassifier(n,p,k)
-
+n = {0:[(1.70,65,20),(1.73,74,24),(1.75,69,25)], 
+              1:[(1.90,85,33),(1.78,76,31),(1.81,75,35)],
+              2:[(1.73,70,75),(1.80,71,63)]
+             } 
+  
+    # testing point p(x,y) 
+    p = (1.90,85,33) 
+  
+    # Number of neighbours  
+    k = 3
+  
+    print("The value classified to unknown point is: {}",KNNClassifier(n,p,k))
+    
+ 
